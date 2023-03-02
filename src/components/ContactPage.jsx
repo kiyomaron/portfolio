c43487587c549ef31fadfb13589d1dd256025662
kiyomaron <kyn.maron947@gmail.com>
2023-03-02 23:52:38 +0900

    Add contents into contact page

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { FaEnvelope, FaGithub } from "react-icons/fa";
 
 const ContactPage = () => {
   return (
-    <div>
+    <section className='contact'>
       <p>お問い合わせはTwitterまたはLinkedInからお願いいたします。</p>
-      
-    </div>
+      <div>
+          <div className='sns-icon'><FaEnvelope /></div>
+          <p>kyn.maron947@gmail.com</p>
+      </div>
+      <div>
+          <a href="https://github.com/kiyomaron" className='sns-icon' target='_blank'><FaGithub /></a>
+          <p>https://github.com/kiyomaron</p>
+      </div>
+    </section>
   );
 };
 
